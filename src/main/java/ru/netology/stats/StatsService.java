143009528ebc87c8e84402697706e6a080571f42
Maria Vostrikova <vostrikova.maria2017@yandex.ru>
2021-09-05 20:14:43 +0300

    StatsService,java modified

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -13,32 +13,27 @@ public class StatsService {
     }
 
     public long calculateAverage(long[] sales) {
-        long sum = 0;
-        int averageSales = 0;
-        for (long sale : sales) {
-            sum += sale;
-            averageSales = (int) (sum / sales.length);
-        }
-        return averageSales;
+        long sum = calcSum(sales);
+        return sum / sales.length;
     }
 
-    public int findMax(long[] sales) {
-        int currentMax = 0;
-        int month = 2;
+    public long findMax(long[] sales) {
+        long currentMax = 0;
+        long month = 0;
         for (long sale : sales) {
-            if (sale > sales[currentMax]) {
-                currentMax = month;
+            if (sale > sales[(int) currentMax]) {
+                currentMax = month + 2;
             }
             month = month + 1;
         }
         return currentMax + 1;
     }
 
-    public int minSales(long[] sales) {
-        int minMonth = 0;
-        int month = 0;
+    public long minSales(long[] sales) {
+        long minMonth = 0;
+        long month = 0;
         for (long sale : sales) {
-            if (sale < sales[minMonth]) {
+            if (sale < sales[(int) minMonth]) {
                 minMonth = month;
             }
             month = month + 1;
@@ -46,9 +41,9 @@ public class StatsService {
         return minMonth + 1;
     }
 
-    public int calculateMonthsSalesBelowAverage(long[] sales) {
-        int monthQuantity = 0;
-        int averageSales = 15;
+    public long calculateMonthsSalesBelowAverage(long[] sales) {
+        long monthQuantity = 0;
+        long averageSales = calculateAverage(sales);
         for (long sale : sales) {
             if (sale < averageSales) {
                 monthQuantity = monthQuantity + 1;
@@ -58,9 +53,9 @@ public class StatsService {
         return monthQuantity;
     }
 
-    public int calculateMonthsSalesAboveAverage(long[] sales) {
-        int monthQuantity = 0;
-        int averageSales = 15;
+    public long calculateMonthsSalesAboveAverage(long[] sales) {
+        long monthQuantity = 0;
+        long averageSales = calculateAverage(sales);
         for (long sale : sales) {
             if (sale > averageSales) {
                 monthQuantity = monthQuantity + 1;
